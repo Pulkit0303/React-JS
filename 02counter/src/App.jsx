cd0ee317e55dd53bd4484598dcdcb0588877d84b
Pulkit1903 <pulkit.19.2003@gmail.com>
2025-08-21 16:53:44 +0530

    Added own functionality of ColorMode

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -2,7 +2,19 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  //et counter = 5
+  let [BGC,setBGC] = useState('0')
+  const darkMode = () =>{
+    if(BGC!=="#121212"){
+      setBGC('#121212')
+    }
+  }
+  const lightMode = () =>{
+    if(BGC!=="#dbd8d8ff"){
+      setBGC('#dbd8d8ff')
+    }
+  }
+
+  
   let [count,setCount] = useState(0)
   const addValue = ()=>{
     if(count<20){
@@ -11,6 +23,7 @@ function App() {
     }
     // setCount(count = count + 1)
   }
+
   const removeValue = () =>{
     if(count>0){
       count -= 1
@@ -18,16 +31,22 @@ function App() {
     }
     // setCount(count = count - 1) can give like this
   }
+
   return (
-    <>
+    <div style={{backgroundColor: BGC, padding: '30px', borderRadius: '20px'}}>
     <h1>Chai aur React</h1>
     <h2>Counter Value: {count}</h2>
     <button onClick={addValue}>Add Value</button>
     <br/>
     <button onClick={removeValue}> Remove Value</button>
     <br/>
+    <button onClick={darkMode}>DarkMode</button>
+    <br/>
+    <button onClick={lightMode}>LightMode</button>
+    <br/>
+    <br/>
     <p>footer: {count}</p>
-    </>
+    </div>
 
   )
 }
