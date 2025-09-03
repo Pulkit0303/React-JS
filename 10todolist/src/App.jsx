79aecd95fc54b475e4640639f4648498153cea99
Pulkit1903 <pulkit.19.2003@gmail.com>
2025-09-03 12:58:41 +0530

    Learned about Context API and localstorage through TodoList

---
+++ 10todolist/src/App.jsx
@@ -0,0 +1,69 @@
+import { useEffect, useState } from "react";
+import { TodoContextProvider, useTodo } from "./context/index";
+import TodoForm from "./components/TodoForm"
+import TodoItem from './components/TodoItem'
+
+function App() {
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (todo) => {
+    setTodos((old) => [
+      {
+        id: Date.now(),
+        ...todo,
+      },
+      ...old,
+    ]);
+  };
+
+  const editTodo = (id, todo) => {
+    setTodos((old) =>
+      old.map((oldTodo) => (oldTodo.id === id ? todo : oldTodo))
+    );
+  };
+
+  const deleteTodo = (id) => {
+    setTodos((old) => old.filter((oldTodo) => oldTodo.id !== id));
+  };
+
+  const toggleComplete = (id) => {
+    setTodos((old)=>(old.map((oldTodo)=>(oldTodo.id === id ? {...oldTodo,completed: !oldTodo.completed}:oldTodo))))
+  };
+
+  useEffect(()=>{
+    const todos = JSON.parse(localStorage.getItem("todos"))
+    if(todos && todos.length > 0){
+      setTodos(todos)
+    }
+  },[])
+
+  useEffect(()=>{
+    localStorage.setItem("todos",JSON.stringify(todos))
+  },[todos])
+
+  return (
+    <TodoContextProvider
+      value={{ todos, addTodo, editTodo, deleteTodo, toggleComplete }}
+    >
+      <div className="bg-[#172842] min-h-screen py-8">
+        <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
+          <h1 className="text-2xl font-bold text-center mb-8 mt-2">
+            Manage Your Todos
+          </h1>
+          <div className="mb-4">
+            <TodoForm />
+          </div>
+          <div className="flex flex-wrap gap-y-3">
+            {todos.map((todo)=>(
+              <div key = {todo.id}className="w-full">
+                <TodoItem todo={todo}/>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </TodoContextProvider>
+  );
+}
+
+export default App;
